45469839feb0bd2c3ec6ce3e1dc554e6dfdf3d39
Shah77966 <shahkhalid.77966@gmail.com>
2023-06-21 23:11:38 +0530

    model over fitting

---
+++ Image_classification.py
@@ -0,0 +1,160 @@
+import tensorflow as tf
+import os
+
+# Avoid OOM errors by setting GPU Memory Consumption Growth
+gpus = tf.config.experimental.list_physical_devices('GPU')
+print("====================GPU's List============================")
+print("GPUS : ", gpus)
+print("================================================\n")
+for gpu in gpus: 
+    tf.config.experimental.set_memory_growth(gpu, True)
+
+tf.config.list_physical_devices('GPU')
+
+## Remove dodgy images
+import cv2
+import imghdr
+
+"""
+data_dir = 'data' 
+image_exts = ['jpeg','jpg', 'bmp', 'png']
+
+for image_class in os.listdir(data_dir): #loop over dirs in data i,e  happy and sad folders
+    for image in os.listdir(os.path.join(data_dir, image_class)): #loop over images in dirs
+        image_path = os.path.join(data_dir, image_class, image)     # getting path of image
+        try: 
+            img = cv2.imread(image_path)    # Read Image
+            tip = imghdr.what(image_path)   # Image extension
+            if tip not in image_exts: 
+                print('Image not in ext list {}'.format(image_path))
+                os.remove(image_path)
+        except Exception as e: 
+            print('Issue with image {}'.format(image_path))
+            # os.remove(image_path)
+"""
+            
+## Load Data
+import numpy as np
+from matplotlib import pyplot as plt
+data = tf.keras.utils.image_dataset_from_directory('data') # Its a generator function
+data_iterator = data.as_numpy_iterator() # So Iterate data
+batch = data_iterator.next()    # Get batch
+# batch[0] is images list and batch[1] contains labels
+# print('batch[0] shape: ', batch[0].shape)
+# print('batch[1] : ', batch[1])
+
+# Now we will check which flag (0 or 1) is assigned to which class.
+# We can modify the flags in tf.keras.dataset configs.
+
+# Visualization
+# fig, ax = plt.subplots(ncols=4, figsize=(20,20))
+# for idx, img in enumerate(batch[0][:4]):
+#     ax[idx].imshow(img.astype(int))
+#     ax[idx].title.set_text(batch[1][idx])
+# plt.show()
+
+# Happy 0 and Sad 1.
+
+## PreProcessing
+# Dividing images by 255
+data = data.map(lambda x,y: (x/255,y))  # x=images, y=labels doing for each image in data
+batch = data.as_numpy_iterator().next()
+
+#Visualizing
+# fig, ax = plt.subplots(ncols=4, figsize=(20,20))
+# for idx, img in enumerate(batch[0][:4]):
+#     ax[idx].imshow(img) # remove int, because data will be in float(between 0 and 1)
+#     ax[idx].title.set_text(batch[1][idx])
+# plt.show()
+
+##Split Data
+print(len(data))    #80
+train_size = int(len(data)*.85) #8
+val_size = int(len(data)*.1)    #4
+test_size = int(len(data)*.05)
+print("Train", train_size, "Val", val_size, "Test", test_size)
+
+# Taking the data and allocation
+train = data.take(train_size)
+val = data.skip(train_size).take(val_size)
+test = data.skip(train_size+val_size).take(test_size)
+
+## Build Deep Learning Model
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
+
+model = Sequential()
+model.add(Conv2D(16, (3,3), 1, activation='relu', input_shape=(256,256,3)))
+model.add(MaxPooling2D())
+model.add(Dropout(0.2))
+
+model.add(Conv2D(32, (3,3), 1, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Dropout(0.2))
+
+model.add(Conv2D(16, (3,3), 1, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Dropout(0.2))
+
+model.add(Flatten())
+
+model.add(Dense(256, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+model.compile('adam', loss=tf.losses.BinaryCrossentropy(), metrics=['accuracy'])
+model.summary()
+
+
+##Train
+logdir='logs'
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
+hist = model.fit(train, epochs=20, validation_data=val, callbacks=[tensorboard_callback])
+
+##Plot Performance
+
+#Loss
+fig = plt.figure()
+plt.plot(hist.history['loss'], color='teal', label='loss')
+plt.plot(hist.history['val_loss'], color='orange', label='val_loss')
+fig.suptitle('Loss', fontsize=20)
+plt.legend(loc="upper left")
+plt.show()
+
+#Accuracy
+fig = plt.figure()
+plt.plot(hist.history['accuracy'], color='teal', label='accuracy')
+plt.plot(hist.history['val_accuracy'], color='orange', label='val_accuracy')
+fig.suptitle('Accuracy', fontsize=20)
+plt.legend(loc="upper left")
+plt.show()
+
+from tensorflow.keras.metrics import Precision, Recall, BinaryAccuracy
+pre = Precision()
+re = Recall()
+acc = BinaryAccuracy()
+for batch in test.as_numpy_iterator(): 
+    X, y = batch
+    yhat = model.predict(X)
+    pre.update_state(y, yhat)
+    re.update_state(y, yhat)
+    acc.update_state(y, yhat)
+print(pre.result(), re.result(), acc.result())
+
+
+## Test
+img = cv2.imread('happy_pic.jpg')
+plt.imshow(img)
+plt.show()
+resize = tf.image.resize(img, (256,256))
+plt.imshow(resize.numpy().astype(int))
+plt.show()
+yhat = model.predict(np.expand_dims(resize/255, 0))
+if yhat > 0.5: 
+    print(f'Predicted class is Sad')
+else:
+    print(f'Predicted class is Happy')
+
+## Save the Model
+from tensorflow.keras.models import load_model
+model.save(os.path.join('models','imageclassifier.h5'))
+new_model = load_model('imageclassifier.h5')
+new_model.predict(np.expand_dims(resize/255, 0))
